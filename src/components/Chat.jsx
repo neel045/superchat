5a73ce1a9af5f44a497cc607f104b8545ef8a8e8
Neel Patel <patelneel0445@gmail.com>
2023-02-06 16:36:32 +0530

    final commit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,14 +7,17 @@ import {
     serverTimestamp,
     where,
 } from "firebase/firestore"
-import { format } from "date-fns"
+import { daysToWeeks, format } from "date-fns"
 import { memo, useEffect, useRef, useState } from "react"
-import { auth, db } from "../../firebase.config"
+import { auth, db, storage } from "../../firebase.config"
+import { getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage"
+import { nanoid } from "nanoid"
 
 const Chat = ({ room }) => {
     const [newMessage, setNewMessage] = useState("")
     const [messages, setMessages] = useState([])
     const lastMessage = useRef(null)
+    const imageInputRef = useRef(null)
 
     const messagesCollection = collection(db, "messages")
 
@@ -41,11 +44,35 @@ const Chat = ({ room }) => {
         }
     }, [messages])
 
+    const uploadImage = async () => {
+        const currentFilePath = imageInputRef?.current?.value
+        if (currentFilePath) {
+            const fileExt = currentFilePath.split(".").pop()
+            const fileName = `img-${auth.currentUser.uid}-${nanoid()}.${fileExt}`
+            const imageRef = ref(storage, `images/${fileName}`)
+
+            try {
+                return new Promise((resolve, reject) =>
+                    uploadBytes(imageRef, currentFilePath).then((data) => {
+                        getDownloadURL(ref(storage, imageRef))
+                            .then((val) => resolve(val))
+                            .catch(reject(null))
+                    })
+                )
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        return null
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault()
-        if (newMessage === "") return
+        const img = await uploadImage()
+        console.log({ img })
+        if (newMessage === "" && !img) return
 
-        await addDoc(messagesCollection, {
+        const msg = {
             text: newMessage.trim(),
             createdAt: serverTimestamp(),
             user: {
@@ -53,7 +80,9 @@ const Chat = ({ room }) => {
                 id: auth.currentUser.uid,
             },
             room,
-        })
+            img,
+        }
+        await addDoc(messagesCollection, msg)
         setNewMessage("")
     }
 
@@ -62,31 +91,34 @@ const Chat = ({ room }) => {
             <h3 className="room-header">Room Id: {room.toUpperCase()}</h3>
 
             <div className="chat-area">
-                {messages &&
-                    messages.map((msg, i) => {
-                        return (
-                            <div
-                                id={msg.id}
-                                key={msg.id}
-                                className={`msg ${
-                                    msg.user.id == auth.currentUser.uid ? "right" : "left"
-                                }`}
-                                ref={lastMessage}
-                            >
-                                <p className="msg-user-name">{msg.user.name}</p>
-                                <p className="msg-body">
-                                    <span className="msg-text">{msg.text} </span>
-                                    <span className="msg-time">
-                                        {msg.createdAt &&
-                                            format(
-                                                new Date(msg.createdAt.seconds * 1000),
-                                                "hh:mm a"
-                                            )}
-                                    </span>
-                                </p>
-                            </div>
-                        )
-                    })}
+                <div className="messages-area">
+                    {messages &&
+                        messages.map((msg, i) => {
+                            return (
+                                <div
+                                    id={msg.id}
+                                    key={msg.id}
+                                    className={`msg ${
+                                        msg.user.id == auth.currentUser.uid ? "right" : "left"
+                                    }`}
+                                    ref={lastMessage}
+                                >
+                                    <p className="msg-user-name">{msg.user.name}</p>
+                                    <p className="msg-body">
+                                        <span className="msg-text">{msg.text} </span>
+                                        <span className="msg-time">
+                                            {msg.createdAt &&
+                                                format(
+                                                    new Date(msg.createdAt.seconds * 1000),
+                                                    "hh:mm a"
+                                                )}
+                                        </span>
+                                    </p>
+                                    <div>{msg?.img && <img src={msg.img} alt="" />}</div>
+                                </div>
+                            )
+                        })}
+                </div>
                 <form onSubmit={handleSubmit} className="new-chat-form">
                     <input
                         type="text"
@@ -97,6 +129,11 @@ const Chat = ({ room }) => {
                         className="inp"
                         autoComplete="off"
                     />
+
+                    <label id="send-img-lable" htmlFor="send-image">
+                        📷
+                    </label>
+                    <input type="file" className="inp" id="send-image" ref={imageInputRef} />
                     <button className="btn send-msg" type="submit">
                         Send
                     </button>
